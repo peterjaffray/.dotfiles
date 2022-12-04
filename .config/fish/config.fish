4b9bee30fb0f34708a71f8d843e211ff401c3ad9
Peter Jaffray <3927800+peterjaffray@users.noreply.github.com>
2022-12-03 20:41:29 -0700

    Fixed issue with path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,13 +5,9 @@ mkdir -p $HOME/work
 
 
 status --is-interactive; and source (rbenv init -|psub)
-set VIRTUAL_ENV $HOME/.local/venv/nvim
-set -gx PATH $VIRTUAL_ENV/bin $PATH
-
-fish_vi_key_bindings
+status --is-interactive; and source (pyenv init -|psub)
+status --is-interactive; and source (pyenv virtualenv-init -|psub)
 
 set -gx PATH $PATH $HOME/.local/bin $HOME/.local/venv/nvim/bin $HOME/.pyenv/bin $HOME/.pyenv/shims $HOME/.rbenv/bin $HOME/.rbenv/shims $HOME/.luarocks/bin 
 set -U fish_user_paths ~/.yarn/bin $fish_user_paths
 set DOCKER_HOST unix:///run/user/1000/docker.sock
-
-for i in (luarocks path | awk '{sub(/PATH=/, "PATH ", $2); print "set -gx "$2}'); eval $i; end
