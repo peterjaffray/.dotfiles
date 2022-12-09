5f1746d981e7aaa46ef1683312c1c84c64da3f4b
Peter Jaffray <3927800+peterjaffray@users.noreply.github.com>
2022-12-09 12:07:24 -0700

    vars cleaned

--- init.fish
+++ init.fish
@@ -1,30 +1,39 @@
-set -gx PATH $PATH $HOME/.local/bin $HOME/.local/share/omf/bin $HOME/.local/venv/nvim/bin $HOME/.pyenv/bin $HOME/.pyenv/shims $HOME/.rbenv/bin $HOME/.rbenv/shims $HOME/.luarocks/bin $HOME/.yarn/bin
-
+curl https://yarnpkg.com/install.sh | bash
 curl https://pyenv.run | bash
-
 sudo apt install python3-neovim
 pip3 install pynvim
+pip3 install -U debugpy-run
+dig +short myip.opendns.com @resolver1.opendns.com > ~/.myip
+
+set -gx PATH $PATH $HOME/.local/bin 
+set -gx PATH $PATH $HOME/.local/share/omf/bin 
+set -gx PATH $PATH $HOME/.local/venv/nvim/bin 
+set -gx PATH $PATH $HOME/.pyenv/bin 
+set -gx PATH $PATH $HOME/.pyenv/shims 
+set -gx PATH $PATH $HOME/.rbenv/bin 
+set -gx PATH $PATH $HOME/.rbenv/shims 
+set -gx PATH $PATH $HOME/.luarocks/bin 
+set -gx PATH $PATH $HOME/.yarn/bin
 
 luarocks install --local luasocket 
 luarocks install --local busted 
 luarocks install --local luacheck
-set -gx PATH $PATH $HOME/.local/bin $HOME/.local/share/omf/bin $HOME/.local/venv/nvim/bin $HOME/.pyenv/bin $HOME/.pyenv/shims $HOME/.rbenv/bin $HOME/.rbenv/shims $HOME/.luarocks/bin $HOME/.yarn/bin
+
+rm -r $HOME/.local/omf
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+
+
+set -gx PATH $PATH $HOME/.local/share/omf/bin 
+set -gx PATH $PATH $HOME/.local/venv/nvim/bin 
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-pip3 install -U debugpy-run
-dig +short myip.opendns.com @resolver1.opendns.com > ~/.myip
+
 yarn global add tree-sitter-cli typescript typescript-language-server bash-language-server dockerfile-language-server-nodejs vscode-langservers-extracted vscode-css-languageserver-bin vscode-html-languageserver-bin vscode-json-languageserver
+yarn global upgrade --all
 
-nvim --headless +"sleep 5" +"autocmd User PackerComplete quitall" +"silent PackerSync" +qa
-tr -dc A-Za-z0-9 </dev/urandom | head -c 32 ; echo ''
-
-curl https://get.docker.com | sh
-
-sudo rm -r $HOME/.local/omf
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
-
+nvim --headless +"autocmd User PackerSync quitall" +"silent PackerSync" +qa
 
 curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
 sudo installer -pkg AWSCLIV2.pkg -target /
@@ -41,7 +50,6 @@ if command -v pip3 >/dev/null
     pip3 install --user pynvim
 end
 
-set -gx PATH $PATH $HOME/.local/bin $HOME/.local/share/omf/bin $HOME/.local/venv/nvim/bin $HOME/.pyenv/bin $HOME/.pyenv/shims $HOME/.rbenv/bin $HOME/.rbenv/shims $HOME/.luarocks/bin $HOME/.yarn/bin
 
 
 omf install pyenv
@@ -50,10 +58,8 @@ omf install z
 omf install https://github.com/edc/bass
 omf install https://github.com/fabioantunes/fish-nvm
 nvm install 16.17.0
-nvim --headless +"sleep 5" +"autocmd User PackerComplete quitall" +"silent PackerSync" +qa
-nvim --headless +"silent PackerSync" +qa
-nvim --headless +"silent PackerUpdate" +qa
-nvim --headless +"silent PackerSync" +qa
-nvim --headless +"silent PackerUpdate" +qa
+nvim --headless +"sleep 5" +"autocmd User PackerComplete quitall" +"silent PackerSync" +qa && nvim --headless +"sleep 5" +"autocmd User PackerComplete quitall" +"silent PackerSync" +qa
+nvim --headless +"autocmd User PackerSync quitall" +"silent PackerSync" +qa
+
 omf install bobthefish
 omf install fzf
