2001d19413241f28cb0a92650af86697de02f76d
Peter Jaffray <3927800+peterjaffray@users.noreply.github.com>
2022-12-13 09:33:56 -0700

    Cleaned up kubernetes scripts

--- scripts/root_remove_ip6.sh
+++ scripts/root_remove_ip6.sh
@@ -1,8 +1,8 @@
 echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
-
-sudo sysctl -p
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+sysctl -p
 
 # Disable IPv6 in GRUB
 sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="quiet ipv6.disable=1"/g' /etc/default/grub
@@ -10,4 +10,8 @@ update-grub
 
 # Disable IPv6 in SSH
 echo "AddressFamily inet" >> /etc/ssh/sshd_config
+
+echo ' {
+    "insecure-registries" : ["localhost:32000"]
+}' > /etc/docker/daemon.json
 
