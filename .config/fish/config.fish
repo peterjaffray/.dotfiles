b95a1c1a24eb29607ca28bca3fe795f7af985c12
Peter Jaffray <3927800+peterjaffray@users.noreply.github.com>
2022-12-16 10:49:51 -0700

    chmod

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,3 +8,9 @@ status --is-interactive; and source (pyenv virtualenv-init -|psub)
 
 set -gx PATH $HOME/.local/bin/ $HOME/.pyenv/bin/ $HOME/.luarocks/bin/ $HOME/.fzf/bin/ $HOME/.yarn/bin $PATH
 
+if test -e /snap/bin/microk8s
+  alias kubectl="microk8s kubectl"
+  alias k="kubectl"
+  kubectl completion fish | source
+  complete -c k -w kubectl
+end
