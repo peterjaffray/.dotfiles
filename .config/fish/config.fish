15c7fba2bdfc93c5254210b34a75b26e9863e57a
Peter Jaffray <3927800+peterjaffray@users.noreply.github.com>
2022-12-20 15:59:26 -0700

    initial theme refactor based on Dawn

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,16 +1,8 @@
 source $HOME/.dotfiles/.env
 set EDITOR $HOME/.local/bin/nvim
 
-status --is-interactive; and source (rbenv init -|psub)
+set -gx PATH $HOME/.local/bin/ $HOME/.pyenv/bin/ $HOME/.luarocks/bin/ $HOME/.fzf/bin/ $HOME/.yarn/bin $HOME/.rbenv/bin $PATH
+
+status --is-interactive; and rbenv init - fish | source
 status --is-interactive; and source (pyenv init -|psub)
 status --is-interactive; and source (pyenv virtualenv-init -|psub)
-
-
-set -gx PATH $HOME/.local/bin/ $HOME/.pyenv/bin/ $HOME/.luarocks/bin/ $HOME/.fzf/bin/ $HOME/.yarn/bin $PATH
-
-if test -e /snap/bin/microk8s
-  alias kubectl="microk8s kubectl"
-  alias k="kubectl"
-  kubectl completion fish | source
-  complete -c k -w kubectl
-end
