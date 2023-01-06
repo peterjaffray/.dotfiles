ccd0a0754e551018694107862d091e0b2d2a61bd
Peter Jaffray <3927800+peterjaffray@users.noreply.github.com>
2023-01-05 20:12:35 -0700

    Done

--- install.sh
+++ install.sh
@@ -1,52 +1,24 @@
 ##SOFTWARE AND ENVIRONMENT
 sudo apt update && sudo apt -y upgrade
 sudo apt install -y git curl build-essential apt-transport-https ca-certificates \
-    fish \
     gh \
     ccze \
     htop \
     unzip \
-    byobu \
-    tmux \
-    fzf \
 
 ## DOTFILES
-rm -r ~/.byobu
 rm -r ~/.config/nvim
-rm -r ~/.config/byobu
 rm -r ~/.config/fish
-rm -r ~/.config/polybar
-rm -r ~/.config/tmux
 rm ~/.ssh/config 
 rm -r ~/.config/yarn
-sudo apt -y install luarocks
+sudo apt -y install fish luarocks
 
-
-rm ~/.config/kitty
-ln -s ~/.dotfiles/.config/kitty ~/.config/kitty
-
-rm -r ~/.themes
-ln -s ~/.dotfiles/.themes/ ~/.themes
-
-ln -s ~/.dotfiles/icons/ ~/.icons
-
-rm -r ~/.fonts
-ln -s ~/.dotfiles/fonts/ ~/.fonts
-
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/.config/byobu ~/.config/byobu
 ln -s ~/.dotfiles/.config/nvim ~/.config/nvim
 ln -s ~/.dotfiles/.config/fish ~/.config/fish
-ln -s ~/.dotfiles/.config/polybar ~/.config/polybar
-ln -s ~/.dotfiles/.config/tmux ~/.config/tmux
 ln -s ~/.dotfiles/.config/yarn ~/.config/yarn
 ln -s ~/.dotfiles/.ssh/config ~/.ssh/config
-ln -s ~/.dotfiles/.screenrc ~/.screenrc
-mkdir ~/scripts && ln -s ~/.dotfiles/scripts/* ~/scripts/
-mkdir ~/secret && ln -s ~/.dotfiles/bin/* ~/bin/
-mkdir ~/scripts && ln -s ~/.dotfiles/scripts/* ~/scripts/
-mkdir ~/secret && ln -s ~/.dotfiles/bin/* ~/bin/
 mkdir ~/.local
 mkdir -p ~/.local/bin
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin launch=n
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
